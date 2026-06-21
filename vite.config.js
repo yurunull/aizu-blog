e4feefed5a566ed634926110c9c5b96640dd9fa2
yurunull <kamomeyuru@gmail.com>
2026-06-21 20:03:06 +0900

    name変更

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { plugin as markdown } from "vite-plugin-markdown";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/AizuBlog/', 
+  base: '/aizu-blog/', 
   plugins: [
     react(),
     markdown({ mode: ['html', 'toc'] })
