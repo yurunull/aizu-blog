bd3517715288728206109f33259dd007267156b6
yurunull <kamomeyuru@gmail.com>
2026-06-22 14:59:26 +0900

    パスをもとの状態に戻した

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { plugin as markdown } from "vite-plugin-markdown";
 
-// https://vite.dev/config/
 export default defineConfig({
-  // Cloudflare Pages用にベースパスを常に "/" に固定します
-  base: "/",
   plugins: [
     react(),
     markdown({ mode: ['html', 'toc'] })
